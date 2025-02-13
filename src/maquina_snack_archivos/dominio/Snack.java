5df7a17c3ea7d34c990bd9750759fcdbf010f2ff
Arturo Valdez <santiguardado27@gmail.com>
2025-02-12 18:08:54 -0600

    Arquitectura multicapas maquina_snack_archivos

---
+++ src/maquina_snack_archivos/dominio/Snack.java
@@ -0,0 +1,71 @@
+package maquina_snack_archivos.dominio;
+import java.io.Serializable;
+import java.util.Objects;
+
+//Uso de javabeans, componentes reutilizables en Java que se
+//utilizan para encapsular datos y comportamientos en aplicaciones
+public class Snack implements Serializable {
+    //Atributos
+    private static int contadorSnacks =0;
+    private int idSnack;
+    private String nombre;
+    private double precio;
+
+    //Constructor vacio por JavaBeans
+    public Snack(){
+        this.idSnack = ++Snack.contadorSnacks;
+    }
+
+    public Snack(String nombre, double precio){
+        this();//manda a llamar constructor vacio
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdSnack() {
+        return idSnack;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    //quals: Compara si dos objetos son iguales según los campos idSnack, nombre y precio.
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    //hashCode: Devuelve un valor hash consistente con la implementación de equals, utilizando los mismos campos para generar el valor hash.
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, nombre, precio);
+    }
+}
---
+++ src/maquina_snack_archivos/servicio/Snacks.java
@@ -0,0 +1,37 @@
+package maquina_snack_archivos.servicio;
+
+import maquina_snack_archivos.dominio.Snack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snacks {
+    private static final List<Snack> snacks;
+
+//    bloque static inicializador
+    static{
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Papas", 20));
+        snacks.add(new Snack("Refresco", 25));
+        snacks.add(new Snack("Torta", 80));
+    }
+
+    public static void agregarSnack(Snack snack){
+        snacks.add(snack);
+    }
+
+    public static void mostrarSacks(){
+        var inventarioSnack = "";
+        for(var snack : snacks){
+            inventarioSnack += snack.toString() + "\n";
+        }
+        System.out.println("--- Snacks en el inventario ---");
+        System.out.println(inventarioSnack);
+    }
+
+    public static List<Snack> getSnacks(){
+        return snacks;
+    }
+
+
+}
